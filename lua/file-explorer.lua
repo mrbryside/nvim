5839d4f7d11604953220fb94af20e683a8e47f25
Sirawat <sirawat@Sirawats-MacBook-Pro.local>
2023-05-03 10:02:44 +0700

    add new update file exploler

--- lua/file-explorer.lua
+++ lua/file-explorer.lua
@@ -15,8 +15,41 @@ end
 
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
+local function on_attach(bufnr)
+  local api = require('nvim-tree.api')
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  api.config.mappings.default_on_attach(bufnr)
+
+  vim.keymap.set('n', '<2-RightMouse>', '', { buffer = bufnr })
+  vim.keymap.del('n', '<2-RightMouse>', { buffer = bufnr })
+  vim.keymap.set('n', 'D', '', { buffer = bufnr })
+  vim.keymap.del('n', 'D', { buffer = bufnr })
+  vim.keymap.set('n', 'E', '', { buffer = bufnr })
+  vim.keymap.del('n', 'E', { buffer = bufnr })
+
+  -- custom key
+  vim.keymap.set('n','l', api.node.open.edit, opts('Edit'))
+  vim.keymap.set('n','h', api.tree.close, opts('close'))
+  vim.keymap.set('n','v', api.node.open.vertical, opts('close'))
+  -- end
+  vim.keymap.set('n', 'A', api.tree.expand_all, opts('Expand All'))
+  vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
+  vim.keymap.set('n', 'C', api.tree.change_root_to_node, opts('CD'))
+  vim.keymap.set('n', 'P', function()
+    local node = api.tree.get_node_under_cursor()
+    print(node.absolute_path)
+  end, opts('Print Node Path'))
+
+  vim.keymap.set('n', 'Z', api.node.run.system, opts('Run System'))
+end
+
 -- empty setup using defaults
 require("nvim-tree").setup({
+  on_attach = on_attach,
   disable_netrw = true,
   hijack_netrw = true,
   respect_buf_cwd = true,
@@ -46,16 +79,16 @@ require("nvim-tree").setup({
     -- dotfiles = true,
 	custom = {"^.git$"}
   },
-  view = {
-	  mappings = {
-		  custom_only = false,
-		  list = {
-			  { key = { "l", "<CR>", "o" }, cb = tree_cb("edit") },
-			  { key = "h", cb = tree_cb("close_node") },
-		      { key = "v", cb = tree_cb("vsplit")}
-		}
-	  }
-  },
+  -- view = {
+  -- 	  mappings = {
+  -- 		  custom_only = false,
+  -- 		  list = {
+  -- 			  { key = { "l", "<CR>", "o" }, cb = tree_cb("edit") },
+  -- 			  { key = "h", cb = tree_cb("close_node") },
+  -- 		      { key = "v", cb = tree_cb("vsplit")}
+  -- 		}
+  -- 	  }
+  -- },
   renderer = {
         highlight_git = true,
         root_folder_modifier = ":t",
